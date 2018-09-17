13e485721e4ce2e1cf6fadb16a9a6c85c140a094
FoxGeoff <fox.geoffrey@gmail.com>
2018-09-17 12:39:36 -0700

    Code the Hero Service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+//
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';  // URL to web api
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+     /* private messageService: MessageService */) { }
+
+  /* Old function for mock heros
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
+  */
+
+  /** GET heroes from the server */
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+  }
+
+
 }
